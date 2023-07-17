6898d5dd31e748304695944ad20ed4f07fc2ca4c
Patton <pattongrooms@rocketmail.com>
2023-07-17 13:07:18 -0400

    commit 20

--- client/src/pages/Tour.jsx
+++ client/src/pages/Tour.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
 const Tour = () => {
+  const [tours, setTours] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try { 
+        const
+      }
+    }
+  }, [])
+
   return (
     <div>
       <h2>PGA Tour Info Page</h2>
+      <form>
+
+      </form>
     </div>
   )
 }
