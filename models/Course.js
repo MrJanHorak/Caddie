b4ceb8a3d06fe364960d07010b7d5e6beb283e1e
Patton <pattongrooms@rocketmail.com>
2023-07-19 01:49:22 -0400

    commit 28

--- models/Course.js
+++ models/Course.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema
 const courseSchema = new Schema({
   courseName: { type: String, required: true },
   city: { type: String, required: true },
-  state: { type: String, required: true }
-  // par: { type: String, required: true }
+  state: { type: String, required: true },
+  par: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Course', courseSchema)
